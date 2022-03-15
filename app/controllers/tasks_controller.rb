d56fa62e7ea12728bffbdfccab8979269daa41ac
xhostcom <xhostcom@gmail.com>
2022-03-15 07:14:10 +0000

    Gitacora saved 9 changed files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,7 +21,8 @@ class TasksController < ApplicationController
 
   # POST /tasks or /tasks.json
   def create
-    @task = Task.new(task_params.merge({ user_id: current_user.id }))
+    :user_id = current_user.id
+    @task = Task.new(task_params.merge[:user_id])
 
     respond_to do |format|
       if @task.save
