698a365cd85753cdedaf41dffc4990c2edc28898
xhostcom <xhostcom@gmail.com>
2022-03-15 06:58:56 +0000

    Gitacora saved 11 changed files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,8 @@
 
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
-  before_action :authenticate_user!
-
-  # GET /tasks or /tasks.json
+ 
+ # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
   end
