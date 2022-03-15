95bbea840a225a5920f35584697cafa79b730d87
xhostcom <xhostcom@gmail.com>
2022-03-15 07:05:27 +0000

    Gitacora saved 9 changed files

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
-   # Validations
-  validates :user_id, presence: true
-  belongs_to :user
+  # Validations
+  # validates :user_id, presence: true
+  belongs_to :user,  polymorphic: true
 end
